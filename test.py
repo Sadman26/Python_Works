19b4b98f3fa02627b649799b24d3039a83f59630
Sadman26 <fareless.ranger@gmail.com>
2022-06-29 16:51:54 +0600

    Array Elemenet Finding

--- test.py
+++ test.py
@@ -1,3 +1,7 @@
-array=[1,23,4,3]
-array.sort()
-print(array)
+array=[3,23,12,5,5,44,3]
+x=int(input("Enter a number: "))
+for i in range(7):
+    if(array[i]==x):
+        print("Found")
+    else:
+        print("Not Found")
